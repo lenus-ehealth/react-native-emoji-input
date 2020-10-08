8ada72ce89f54d79d4e69443905a8823a72460d3
Marco Sylvestersen <marcosylvestersen@gmail.com>
2020-10-08 14:18:07 +0200

    Remove search functionality, etc

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,31 +1,47 @@
-import _ from 'lodash';
-import fs from 'fs';
+import _ from 'lodash'
+import fs from 'fs'
 
 import {
-    category,
-    categoryIndexMap,
-    emojiLib,
-    emojiMap,
-    emojiArray
-} from '../src/emoji-data';
+  category,
+  categoryIndexMap,
+  emojiLib,
+  emojiArray,
+} from '../src/emoji-data'
 
 let data = {
-    category,
-    categoryIndexMap,
-    emojiLib,
-    emojiMap,
-    emojiArray
-};
+  category,
+  categoryIndexMap,
+  emojiLib,
+}
 
-var stingified = JSON.stringify(data).replace(
-    /(["'])require(?:(?=(\\?))\2.)*?\1/g,
-    value => value.replace(/"/g, '')
-);
+var stingified = JSON.stringify(
+  data
+).replace(/(["'])require(?:(?=(\\?))\2.)*?\1/g, (value) =>
+  value.replace(/"/g, '')
+)
 
 fs.writeFile(
-    'src/emoji-data/compiled.js',
-    `module.exports = ${stingified}`,
-    err => {
-        if (err) throw err;
-    }
-);
+  'src/emoji-data/compiled.js',
+  `module.exports = ${stingified}`,
+  (err) => {
+    if (err) throw err
+  }
+)
+
+let dataSearch = {
+  emojiArray,
+}
+
+var stingified = JSON.stringify(
+  dataSearch
+).replace(/(["'])require(?:(?=(\\?))\2.)*?\1/g, (value) =>
+  value.replace(/"/g, '')
+)
+
+fs.writeFile(
+  'src/emoji-data/compiledSearch.js',
+  `module.exports = ${stingified}`,
+  (err) => {
+    if (err) throw err
+  }
+)
--- src/EmojiSearch.js
+++ src/EmojiSearch.js
@@ -1,22 +1,22 @@
-import Fuse from "fuse.js";
+import Fuse from 'fuse.js'
 
-const { emojiArray } = require("./emoji-data/compiled");
+const { emojiArray } = require('./emoji-data/compiledSearch')
 
-const emojiSynonyms = require("./emoji-data/emojiSynonyms");
+const emojiSynonyms = require('./emoji-data/emojiSynonyms')
 
 // Extend keywords using synonym JSON file
 for (let i = 0; i < emojiArray.length; i++) {
-  emojiArray[i]["keywords"] = emojiArray[i]["keywords"].concat(
-    emojiSynonyms[i]["keywords"]
-  );
+  emojiArray[i]['keywords'] = emojiArray[i]['keywords'].concat(
+    emojiSynonyms[i]['keywords']
+  )
 }
 
 const fuseOptions = {
   shouldSort: true,
   threshold: 0.0, // threshold of 0.0 is perfect match and 1.0 is any match
-  keys: ["keywords"]
-};
+  keys: ['keywords'],
+}
 
-const EmojiSearchSpace = new Fuse(emojiArray, fuseOptions);
+const EmojiSearchSpace = new Fuse(emojiArray, fuseOptions)
 
-export default EmojiSearchSpace;
+export default EmojiSearchSpace
